ca63a436479857847782f29a67e76035c23c5efa
abazure_jp <say.0213@gmail.com>
2017-03-18 23:33:15 +0900

    😉2

--- phantomjs/screenshot.js
+++
@@ -1,8 +0,0 @@
-var page = require('webpage').create();
-page.open('http://example.com', function(status) {
-  console.log("status: " + status);
-  if(status === "success") {
-    page.render('example.png');
-  }
-  phantom.exit();
-});
